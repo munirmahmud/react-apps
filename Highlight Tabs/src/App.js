788899e9390c9ae8b6e884652f8bee0c40b68eb9
Munir Mahmud <munirmahmud6@gmail.com>
2020-12-10 15:15:48 +0600

    Router & navigation link added

--- Highlight Tabs/src/App.js
+++ Highlight Tabs/src/App.js
@@ -1,22 +1,35 @@
 import React from 'react';
+import { BrowserRouter as Router, NavLink, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Tab from './components/Tab';
 
 const App = () => {
-    
-
     return (
-        <div className="app">
-            <div className="browser">
-                <div className="tabs">
-                    <Tab><a>Home</a></Tab>
-                    <Tab><a>About</a></Tab>
-                    <Tab><a>Features</a></Tab>
-                </div>
+        <Router>
+            <div className="app">
+                <div className="browser">
+                    <div className="tabs">
+                        <Tab><NavLink exact to="/">Home</NavLink></Tab>
+                        <Tab><NavLink to="/about">About</NavLink></Tab>
+                        <Tab><NavLink to="/features">Features</NavLink></Tab>
+                    </div>
 
-                <div className="viewport">Pages Go Here</div>
+                    <div className="viewport">
+                        <Switch>
+                            <Route exact path="/">
+                                <Home />
+                            </Route>
+                            <Route path="/about">
+                                <About />
+                            </Route>
+                            <Route path="/features">
+                                <Features />
+                            </Route>
+                        </Switch>
+                    </div>
+                </div>
             </div>
-        </div>
+        </Router>
     );
 };
 
